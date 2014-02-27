23fef55e2102c52c03bbe76de2a87ec2ce4fd6f3
chhernandez <itsadmin@CCSSE-859409.local>
2014-02-27 00:19:13 -0600

    added fashion example 3 demo
    this one is another test of the login process

---
+++ demo_App/FashionExample3/FashionExample/FashionExample/cdkDefaultSettingsViewController.h
@@ -0,0 +1,13 @@
+//
+//  cdkDefaultSettingsViewController.h
+//  FashionExample
+//
+//  Created by itsadmin on 2/26/14.
+//  Copyright (c) 2014 Carlos Hernandez. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface cdkDefaultSettingsViewController : UIViewController
+
+@end
---
+++ demo_App/FashionExample3/FashionExample/FashionExample/cdkDefaultSettingsViewController.m
@@ -0,0 +1,38 @@
+//
+//  cdkDefaultSettingsViewController.m
+//  FashionExample
+//
+//  Created by itsadmin on 2/26/14.
+//  Copyright (c) 2014 Carlos Hernandez. All rights reserved.
+//
+
+#import "cdkDefaultSettingsViewController.h"
+
+@interface cdkDefaultSettingsViewController ()
+
+@end
+
+@implementation cdkDefaultSettingsViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
