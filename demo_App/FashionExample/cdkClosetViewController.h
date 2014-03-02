bcf940ae2b94f161b06193b3e150ca8deb8c6ede
chhernandez <carloshernandez@utexas.edu>
2014-03-02 14:29:14 -0600

    working on subqueries for cloth types
    working on subqueries for types

---
+++ demo_App/FashionExample/cdkClosetViewController.h
@@ -0,0 +1,15 @@
+//
+//  cdkClosetViewController.h
+//  FashionExample
+//
+//  Created by Carlos Hernandez on 3/2/14.
+//  Copyright (c) 2014 Carlos Hernandez. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface cdkClosetViewController : UICollectionViewController
+
+@property (strong, nonatomic) NSArray *typeArray;
+
+@end
